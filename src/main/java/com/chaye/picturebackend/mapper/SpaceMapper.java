093209cfb4bff3c30925f764b8178f623f2e048e
chaye <852635477@qq.com>
2026-01-05 12:48:03 +0800

    Initial commit

---
+++ src/main/java/com/chaye/picturebackend/mapper/SpaceMapper.java
@@ -0,0 +1,18 @@
+package com.chaye.picturebackend.mapper;
+
+import com.chaye.picturebackend.model.entity.Space;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author chaye
+* @description 针对表【space(空间)】的数据库操作Mapper
+* @createDate 2024-12-18 19:53:34
+* @Entity entity.model.com.chaye.picturebackend.Space
+*/
+public interface SpaceMapper extends BaseMapper<Space> {
+
+}
+
+
+
+
---
+++ src/main/java/com/chaye/picturebackend/mapper/SpaceUserMapper.java
@@ -0,0 +1,18 @@
+package com.chaye.picturebackend.mapper;
+
+import com.chaye.picturebackend.model.entity.SpaceUser;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author chaye
+* @description 针对表【space_user(空间用户关联)】的数据库操作Mapper
+* @createDate 2025-01-02 20:07:15
+* @Entity entity.model.com.chaye.picturebackend.SpaceUser
+*/
+public interface SpaceUserMapper extends BaseMapper<SpaceUser> {
+
+}
+
+
+
+
