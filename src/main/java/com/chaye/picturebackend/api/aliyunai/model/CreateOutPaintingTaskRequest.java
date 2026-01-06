4dd0ad1c18e8e5b0d944bde98893c415b471d372
chaye <852635477@qq.com>
2026-01-06 11:36:00 +0800

    将模型配置移动到配置文件中

--- src/main/java/com/chaye/picturebackend/api/aliyunai/model/CreateOutPaintingTaskRequest.java
+++ src/main/java/com/chaye/picturebackend/api/aliyunai/model/CreateOutPaintingTaskRequest.java
@@ -3,6 +3,7 @@ package com.chaye.picturebackend.api.aliyunai.model;
 import cn.hutool.core.annotation.Alias;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.io.Serializable;
 
@@ -15,7 +16,8 @@ public class CreateOutPaintingTaskRequest implements Serializable {
     /**
      * 模型，例如 "image-out-painting"
      */
-    private String model = "image-out-painting";
+    @Value("${aliYunAi.model}")
+    private String model;
 
     /**
      * 输入图像信息
